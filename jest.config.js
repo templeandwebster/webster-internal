44e269accf1516127c87733f548e8d4a612b482b
daddia <jonathan.daddia@templeandwebster.com.au>
2025-03-25 17:06:01 +1100

    Enhance project configuration: update .prettierignore to include additional files, modify jest.config.js to support multiple projects and watch plugins, update package.json to use pnpm@10.6.5, refine turbo.json input patterns, and correct package name in webster-stylelint package.json.

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,13 @@ module.exports = {
   watchPathIgnorePatterns: ['<rootDir>/node_modules/'],
   testPathIgnorePatterns: ['.*/tests/fixtures/'],
   testRegex: '.*\\.test\\.(tsx?|jsx?)$',
+  projects: [
+    '<rootDir>/packages/webster-prettier',
+    '<rootDir>/packages/webster-stylelint',
+    '<rootDir>/packages/webster-typescript',
+  ],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 };
